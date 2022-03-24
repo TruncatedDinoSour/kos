f9ea5692f2e783e2d513b9cf5a8ab0776a645f45
Ari Archer <ari.web.xyz@gmail.com>
2022-03-25 00:09:30 +0200

    Added tests on git commit
    Signed-off-by: Ari Archer <ari.web.xyz@gmail.com>

--- scripts/git.sh
+++ scripts/git.sh
@@ -3,6 +3,9 @@
 set -e
 
 main() {
+    su -c './scripts/test/root.sh'
+    ./scripts/test/noroot.sh
+
     git add -A
     git commit -sam "${m:-"update @ $(date)"}"
     git push -u origin "$(git rev-parse --abbrev-ref HEAD)"
