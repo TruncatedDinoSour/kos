5a4fa5739d969798f7ce1a051a65356f8f4199d7
Ari Archer <ari.web.xyz@gmail.com>
2022-03-25 17:50:32 +0200

    Modify git.sh script to ask for input before commiting
    Signed-off-by: Ari Archer <ari.web.xyz@gmail.com>

--- scripts/git.sh
+++ scripts/git.sh
@@ -6,6 +6,9 @@ main() {
     ${__BASH_DOAS:-kos} ./scripts/test/root.sh
     ./scripts/test/noroot.sh
 
+    printf 'Press enter to continue'
+    read -r
+
     git add -A
     git commit -sam "${m:-"update @ $(date)"}"
     git push -u origin "$(git rev-parse --abbrev-ref HEAD)"
