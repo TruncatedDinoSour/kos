f886111b46545cdd7a19635704e09016b606fefb
Ari Archer <ari.web.xyz@gmail.com>
2022-10-16 00:51:38 +0300

    Pre-42: Remove flags from the bash completions
    Signed-off-by: Ari Archer <ari.web.xyz@gmail.com>

--- completions/kos.bash
+++ completions/kos.bash
@@ -1,17 +1,19 @@
 # kos(1) completion                                    -*- shell-script -*-
 
 _kos() {
-    local words cword cmd
+    local cword cmd
     _init_completion -s || return
 
     for ((cmd = 1; cmd <= cword; cmd++)); do
-        if [[ "${words[cmd]}" != -* ]]; then
-            local PATH=$PATH:/sbin:/usr/sbin:/usr/local/sbin
-            _command_offset $cmd
-            return
-        else
-            COMPREPLY=('--version')
-        fi
+        # if [[ "${words[cmd]}" != -* ]]; then
+
+        local PATH="$PATH:/sbin:/usr/sbin:/usr/local/sbin"
+        _command_offset $cmd
+        # return
+
+        # else
+        #     COMPREPLY=('--version')
+        # fi
     done
 } && complete -F _kos -o bashdefault -o default kos
 
